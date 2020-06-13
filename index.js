e98ee5187da2b3c54c548be7be4f0341d57f48ec
Mason <read_xms@hotmail.com>
2020-06-13 16:12:25 +0800

    add status

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw/root.js', {scope: '/sw/static/'}).then(function (reg) {
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
     // registration worked
-    console.log('Registration succeeded. Scope is ' + reg.scope);
+    // console.log('Registration succeeded. Scope is ' + reg.scope);
   }).catch(function (error) {
     // registration failed
     console.log('Registration failed with ' + error);
