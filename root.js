c30c86ba00fa3c4a291f28ec2e708f7554b2e950
Mason <read_xms@hotmail.com>
2020-06-13 17:36:35 +0800

    test

--- root.js
+++ root.js
@@ -1,7 +1,8 @@
+const VERSION = 'v1'
 self.addEventListener('install', function (event) {
   console.log('sw is installed')
   event.waitUntil(
-    caches.open('v1').then(function (cache) {
+    caches.open(VERSION).then(function (cache) {
       return cache.addAll([
         '/sw/static/index.css',
         '/sw/static/index.js',
@@ -12,16 +13,32 @@ self.addEventListener('install', function (event) {
   );
 });
 
-self.addEventListener('activate', event => {
-  console.log('V1 now ready to handle fetches!');
+// 缓存更新
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          // 如果当前版本和缓存版本不一致
+          if (cacheName !== VERSION) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
+// 捕获请求并返回缓存数据
 self.addEventListener('fetch', function (event) {
-  const url = new URL(event.request.url);
-
-  // serve the cat SVG from the cache if the request is
-  // same-origin and the path is '/dog.svg'
-  if (url.pathname === '/test.png') {
-    event.respondWith(caches.match('/test1.png'));
-  }
+  event.respondWith(caches.match(event.request).catch(function () {
+    return fetch(event.request);
+  }).then(function (response) {
+    caches.open(VERSION).then(function (cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }).catch(function () {
+    return caches.match('./static/test1.png');
+  }));
 });
