cc0a1b0fc6358f591469242790e1e814b0a04677
Mason <read_xms@hotmail.com>
2020-06-13 15:44:40 +0800

    test

--- root.js
+++ root.js
@@ -1,1 +1,12 @@
-console.log('from root')
+this.addEventListener('install', function(event) {
+    console.log('sw is installed')
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        return cache.addAll([
+          '/sw/static/',
+          '/sw/static/style.css',
+          '/sw/static/index.js',
+        ]);
+      })
+    );
+  });
