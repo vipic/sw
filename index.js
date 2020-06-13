600ad38d3b27995dba333d68c64fc72cc4036e48
Mason <read_xms@hotmail.com>
2020-06-13 18:34:20 +0800

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw/root.js', {scope: '/sw/static/'}).then(function (reg) {
+  navigator.serviceWorker.register('/root.js', {scope: '/sw/static/'}).then(function (reg) {
     if(reg.installing) {
       console.log('Service worker installing');
     } else if(reg.waiting) {
