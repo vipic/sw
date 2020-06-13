2d9a7af9188a5ea08b38ef44134385f0afd00326
Mason <read_xms@hotmail.com>
2020-06-13 16:07:02 +0800

    test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw/root.js', { scope: '/sw/static/' }).then(function(reg) {
-      // registration worked
-      console.log('Registration succeeded. Scope is ' + reg.scope);
-    }).catch(function(error) {
-      // registration failed
-      console.log('Registration failed with ' + error);
-    });
-  }
+  navigator.serviceWorker.register('/sw/root.js', {scope: '/sw/static/'}).then(function (reg) {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function (error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
--- root.js
+++ root.js
@@ -1,12 +1,34 @@
-this.addEventListener('install', function(event) {
-    console.log('sw is installed')
-    event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        return cache.addAll([
-          '/sw/index.html',
-          '/sw/static/index.css',
-          '/sw/static/index.js',
-        ]);
+this.addEventListener('install', function (event) {
+  console.log('sw is installed')
+  event.waitUntil(
+    caches.open('v1').then(function (cache) {
+      return cache.addAll([
+        '/sw/static/index.css',
+        '/sw/static/index.js',
+      ]);
+    })
+  );
+});
+
+
+this.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+  ).then((response) => {
+    if (response !== undefined) {
+      return response
+    } else {
+      fetch(event.request).then(res => {
+        let resClone = res.clone();
+
+        caches.open('v1').then((cache) => {
+          cache.put(event.request, resClone)
+        })
+        return res
+      }).catch(err => {
+        console.error(err)
       })
-    );
+    }
+
   });
+});
