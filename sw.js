95a809e9b4cf6ceec8aea3f4431dce78f0b0491a
Mason <read_xms@hotmail.com>
2020-06-14 14:52:03 +0800

    test

--- sw.js
+++ sw.js
@@ -29,25 +29,14 @@ self.addEventListener('activate', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-  const requestUrl = new URL(event.request.url);
-  if(event.request.url.includes('index.html')) {
+  if (event.request.url.includes('index.html')) {
     return fetch(event.request);
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        return response || fetch(event.request);
+      })
+    );
   }
-  if (requestUrl.origin === location.origin) {
-    if (requestUrl.pathname === '/') {
-      event.respondWith(
-        caches.open(VERSION).then(function (cache) {
-            caches.match(event.request).then(function (response) {
-              return response || fetch(event.request);
-            })
-          }
-        )
-      )
-    }
-  }
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
+
 });
