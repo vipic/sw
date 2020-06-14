1709ce8bac5a946af8003dd856f010c040dc2448
Mason <read_xms@hotmail.com>
2020-06-14 21:52:38 +0800

    test

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw/sw/sw.js').then(function (reg) {
-    if(reg.installing) {
-      console.log('Service worker installing');
-    } else if(reg.waiting) {
-      console.log('Service worker installed');
-    } else if(reg.active) {
-      console.log('Service worker active');
-    }
-    // registration worked
-    // console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function (error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/sw/sw/sw.js').then(function (reg) {
+//     if(reg.installing) {
+//       console.log('Service worker installing');
+//     } else if(reg.waiting) {
+//       console.log('Service worker installed');
+//     } else if(reg.active) {
+//       console.log('Service worker active');
+//     }
+//     // registration worked
+//     // console.log('Registration succeeded. Scope is ' + reg.scope);
+//   }).catch(function (error) {
+//     // registration failed
+//     console.log('Registration failed with ' + error);
+//   });
+// }
 function loadImg(){
   const img = document.createElement('img')
   img.src = './static/test.png'
